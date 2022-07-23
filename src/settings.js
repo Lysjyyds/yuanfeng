997058a8f48187114a808835a42361b4610ef037
zjh <zjiahe0124@163.com>
2022-07-23 22:26:59 +0800

    项目初始化

---
+++ src/settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+  title: 'Vue Admin Template', // 项目头部
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false, // 头部是否固定
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false // 是否显示logo
+}
